0e5fb99880fa6c7c9efe3efdcd7fbc4afc7649b9
Casey Haaland <caseychandlerh@yahoo.com>
2019-09-18 01:10:12 +0100

    Bug fixes

--- assets/code/zipf_ols.py
+++ assets/code/zipf_ols.py
@@ -11,6 +11,30 @@ from mpl_toolkits.mplot3d import Axes3D
 pjoin = os.path.join
 
 
+def word_freqs(topk: int = 25):
+    word_counts = Counter() 
+    with open(pjoin("..", "txt", "hamlet.txt"), "rt") as f:
+        text = " ".join(f.readlines())
+        text = re.sub("[^0-9a-zA-Z ]+", "", text)
+        all_words = [word.lower() for word in text.split(" ")]
+        word_counts = Counter(all_words)
+
+    n_words = len(all_words)
+    words, counts = zip(*word_counts.most_common(topk))
+    freqs = np.asarray([c / n_words for c in counts])
+    return words, freqs
+
+def plot_scatter_points(freqs):
+    xs = np.arange(1, 1 + freqs.size)
+    ys = freqs
+    plt.figure(figsize=(10,10))
+    plt.scatter(xs, ys)
+    plt.title(r"Word Frequency vs. Rank")
+    plt.xlabel("Rank", fontsize=14)
+    plt.ylabel("Frequency", fontsize=14)
+    plt.grid(True)
+    plt.savefig(pjoin("..", "images", "shakespeare-freq-scatter.png"))
+
 def fit_zipf_ols(freq_counts_desc: np.ndarray):
     ranks = np.arange(1, 1 + freq_counts_desc.size)
     A = np.stack([np.ones_like(freq_counts_desc), np.log(ranks)], axis=1)
@@ -23,43 +47,6 @@ def fit_zipf_ols(freq_counts_desc: np.ndarray):
     mse = np.sqrt(opt_result.cost)
 
     return C, alpha, mse
-
-# def fit_heaps_ols(n_distinct_words: np.ndarray):
-#     ranks = np.arange(1, 1 + n_distinct_words.size)
-#     A = np.stack([np.ones_like(n_distinct_words), np.log(ranks)], axis=1)
-#     b = np.log(n_distinct_words)
-#     f = lambda x: A @ x - b
-#     opt_result = least_squares(f, x0=np.zeros(2))
-
-#     C = np.exp(opt_result.x[0])
-#     alpha = opt_result.x[1]
-#     mse = np.sqrt(opt_result.cost)
-#     return C, alpha, mse
-
-def word_freqs(topk: int = 25):
-    word_counts = Counter() 
-    with open(pjoin("..", "txt", "hamlet.txt"), "rt") as f:
-        text = " ".join(f.readlines())
-        text = re.sub("[^0-9a-zA-Z]+", " ", text)
-        all_words = [word.lower() for word in text.split(" ")]
-        word_counts = Counter(all_words)
-
-    n_words = len(all_words)
-    words, counts = zip(*word_counts.most_common(topk))
-    freqs = np.asarray([c / n_words for c in counts])
-    return words, freqs
-
-# def n_distinct_words_per_doc_length():
-#     with open(pjoin("..", "txt", "hamlet.txt"), "rt") as f:
-#         text = " ".join(f.readlines())
-#         text = re.sub("[^0-9a-zA-Z]+", " ", text)
-#         counts = []
-#         distinct_words = set()
-#         for w in text.split(" "):
-#             distinct_words.add(w.lower())
-#             counts.append(len(distinct_words))
-
-#     return np.asarray(counts)
 
 def plot_zipf_param_surface(empirical_freqs):
     c_plot = np.linspace(-10, 10, 100)
@@ -77,9 +64,10 @@ def plot_zipf_param_surface(empirical_freqs):
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection="3d")
     ax.plot_surface(C_mesh, Alpha_mesh, Z, cmap="hot")
-    ax.set_title(r"$\sum_{i=1}^N (f_i - C x^\alpha)^2$")
+    ax.set_title(r"$\sum_{i=1}^N (f_i - C x_i^\alpha)^2$")
     ax.set_xlabel(r"$C$", fontsize=14)
     ax.set_ylabel(r"$\alpha$", fontsize=14)
+    plt.savefig(pjoin("..", "images", "shakespeare-zipf-param-surface.png"))
 
 def plot_zipf_transformed_param_surface(empirical_freqs):
     log_c_plot = np.log(np.linspace(0.01, 0.2, 100))
@@ -98,7 +86,19 @@ def plot_zipf_transformed_param_surface(empirical_freqs):
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection="3d")
     ax.plot_surface(C_mesh, Alpha_mesh, Z, cmap="hot")
-    ax.set_title(r"$\sum_{i=1}^N (f_i - \log(C) - \alpha \log(x))^2$")
+    ax.set_title(r"$\sum_{i=1}^N (f_i - \log(C) - \alpha \log(x_i))^2$")
+    plt.savefig(pjoin("..", "images", "shakespeare-zipf-transformed-param-surface.png"))
+
+def plot_transformed_scatter_points(freqs, N):
+    xs = np.log(np.arange(1, 1 + freqs.size))
+    ys = np.log(freqs)
+    plt.figure(figsize=(10,10))
+    plt.scatter(xs, ys)
+    plt.title(r"$\log(f)$ vs. $\log(rank)$")
+    plt.xlabel(r"$\log(rank)", fontsize=14)
+    plt.ylabel(r"$\log(f)", fontsize=14)
+    plt.grid(True)
+    plt.savefig(pjoin("..", "images", "shakespeare-zipf-transformed-param-scatter.png"))
 
 def plot_zipf_transformed_param_contours(empirical_freqs):
     log_c_plot = np.log(np.linspace(0.01, 0.2, 100))
@@ -114,12 +114,15 @@ def plot_zipf_transformed_param_contours(empirical_freqs):
             residual = log_freqs - yhat
             mse = np.sqrt(np.mean(np.square(residual)))
             Z[i, j] = mse
+
     fig = plt.figure(figsize=(10, 10))
     plt.contour(C_mesh, Alpha_mesh, Z, cmap="hot", levels=20)
-    plt.title(r"$\sum_{i=1}^N (f_i - \log(C) - \alpha \log(x))^2$")
-    plt.xlabel(r"$\log(C)$")
-    plt.ylabel(r"$\alpha$")
+    plt.title(r"$\sum_{i=1}^N (f_i - \log(C) - \alpha \log(x_i))^2$")
+    plt.xlabel(r"$\log(C)$", fontsize=14)
+    plt.ylabel(r"$\alpha$", fontsize=14)
+    plt.grid(True)
     plt.tight_layout()
+    plt.savefig(pjoin("..", "images", "shakespeare-zipf-transformed-param-contours.png"))
 
 def plot_ols_zipf_fit(empirical_freqs, N):
     plt.figure(figsize=(10,10))
@@ -134,18 +137,16 @@ def plot_ols_zipf_fit(empirical_freqs, N):
     plt.plot(xs, C_ols * xs ** alpha_ols, "g", linewidth=2, label=rf"$f_{{ols}}(x) = {C_ols:.2}x^{{{alpha_ols:.2}}}$")
     plt.ylim([0, 0.04])
     plt.legend()
-    # plt.savefig(pjoin("..", "images", "shakespeare-word-frequency.png"))
+    plt.tight_layout()
+    plt.savefig(pjoin("..", "images", "shakespeare-zipf-fit.png"))
 
 if __name__ == "__main__":
     N = 100
     most_freq_words, freqs = word_freqs(N)
-    # plot_scatter_points(freqs, N)
+    plot_scatter_points(freqs)
     plot_zipf_param_surface(freqs)
-    # plot_transformed_scatter_points(freqs, N)
+    plot_transformed_scatter_points(freqs, N)
     plot_zipf_transformed_param_contours(freqs)
     plot_zipf_transformed_param_surface(freqs)
 
     plot_ols_zipf_fit(freqs, N)
-    # counts = n_distinct_words_per_doc_length()
-
-    plt.show()
